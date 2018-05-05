b407624fd24d131bca87b1c43e252fe499d9475c
unknown <terebesi.robert@yahoo.com>
2018-05-05 15:06:00 +0300

    First version of market data parsing

--- kraken/src/main/java/com/kraken/examples/Examples.java
+++ kraken/src/main/java/com/kraken/examples/Examples.java
@@ -1,4 +1,32 @@
 package main.java.com.kraken.examples;
 
+import main.java.com.kraken.MarketData.MarketDataParser;
+import main.java.com.kraken.MarketData.Quotation;
+import main.java.com.kraken.api.KrakenApi;
+import main.java.com.kraken.api.KrakenApi.Method;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Examples {
+
+    public static void main(String[] args) throws IOException, InvalidKeyException, NoSuchAlgorithmException {
+
+        KrakenApi api = new KrakenApi();
+
+        String response;
+        Map<String, String> input = new HashMap<>();
+
+        input.put("pair", "XBTEUR");
+        response = api.queryPublic(Method.DEPTH, input);
+        List<Quotation> result = MarketDataParser.parse(response);
+        result.stream().forEach(
+                q -> System.out.println(q)
+        );
+
+    }
 }
